32610ee228216c7e7ae25ddcc47092685955b743
Rachelle <rrosiles@hawk.iit.edu>
2024-02-08 23:04:40 -0600

    bugs!

---
+++ Sine Convergence and Precision Analysis/Operators.swift
@@ -0,0 +1,8 @@
+//
+//  Operators.swift
+//  Sine Convergence and Precision Analysis
+//
+//  Created by Edin Peskovic on 2/8/24.
+//
+
+import Foundation
---
+++ Sine Convergence and Precision Analysis/Sine Calc.swift
@@ -0,0 +1,8 @@
+//
+//  Sine Calc.swift
+//  Sine Convergence and Precision Analysis
+//
+//  Created by Edin Peskovic on 2/8/24.
+//
+
+import Foundation
