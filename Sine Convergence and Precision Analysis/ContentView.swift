4545146cd937d52ced65bc4524f8c8ec16a55094
Rachelle <rrosiles@hawk.iit.edu>
2024-02-08 23:05:02 -0600

    bugs!

--- Sine Convergence and Precision Analysis/ContentView.swift
+++ Sine Convergence and Precision Analysis/ContentView.swift
@@ -5,20 +5,237 @@
 //  Created by Edin Peskovic on 2/8/24.
 //
 
+//
+//  ContentView.swift
+//  Regular-Bessel-Function-Observation
+//
+//  Created by Jeff_Terry on 1/13/24.
+//
+
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var guess = ""
+    @State private var totalInput: Int? = 1 //l -> x
+    //@State var besselResultArray :[(direction: String, xValue: Double, order: Int, start: Int, besselValue: Double)] = []
+    @State var sinResultsArray :[(xValue: Double, terms: Int, sumVal: Double, builtinVal: Double)] = []
+    
+    private var intFormatter: NumberFormatter = {
+        let f = NumberFormatter()
+        f.numberStyle = .decimal
+        return f
+    }()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+    
+       VStack {
+            HStack {
+                
+                TextEditor(text: $guess)
 
-#Preview {
-    ContentView()
-}
+                
+                Button("Calculate Sine", action:
+                    
+                    calculateSinFunc
+                
+                )
+                    }
+            .frame(minHeight: 300, maxHeight: 800)
+            .frame(minWidth: 480, maxWidth: 800)
+            .padding()
+        
+        HStack{
+            
+            Text(verbatim: "Order:")
+            .padding()
+            TextField("Total", value: $totalInput, formatter: intFormatter)
+        
+                .padding()
+            
+            }
+               
+        }
+        
+    }
+
+    
+    func calculateSinFunc()   {
+
+        let xmax = 1.0                    /* max of x  */
+        let xmin = 0.1                     /* min of x >0  */
+        let step = 0.1                      /* delta x  */
+        let terms = totalInput!                      /* terms in summation */
+            //let start = order + 25                      /* used for downward algorithm */
+        //var x = 0.0
+        var maxIndex = 0
+            
+        sinResultsArray.removeAll()
+            
+        guess = String(format: "J%d(x)\n", terms)
+        guess += "Start time \(Date())\n"
+        let startTime : DispatchTime = .now()
+            
+        maxIndex = Int(((xmax-xmin)/step))+1
+
+        let theMaxIndex = maxIndex
+        
+        Task{
+        
+            
+                let _ = await withTaskGroup(of: Void.self /* this is the return from the taskGroup*/,
+                                                                   returning: Void.self /* this is the return of all of the results */,
+                                                                   body: { taskGroup in  /*This is the body of the task*/
+                    
+                    // We can use `taskGroup` to spawn child tasks here.
+                    for index in (0...theMaxIndex)
+                    {
+                        taskGroup.addTask {
+                            
+                            //Start the calculation
+                            await calculateSinSum(index: index, step: step, xmin: xmin, order: terms) /*calculateUpwardDownwardBessel(index: index, step: step, xmin: xmin, order: order, start: start)*/
+                            
+                            
+                           // return index /* this is the return from the taskGroup*/
+                            
+                        }
+                        
+                    }
+                    
+                    
+                    
+                    // Collate the results of all child tasks
+                   // var combinedTaskResults :[Int] = []
+                  //  for await result in taskGroup {
+                        
+                  //      combinedTaskResults.append(result)
+                  //  }
+                    
+                    
+                    
+                  //  return combinedTaskResults  /* this is the return from the result collation */
+                    
+                })
+                
+                //Do whatever processing that you need with the returned results of all of the child tasks here.
+                
+                // Sort the results based upon the direction of the result
+                let sortedFinishedResults = besselResultArray.sorted(by: { $0.1 < $1.1 })
+                
+                await clearBesselArray()
+            
+                await updateBesselArray(array: sortedFinishedResults)
+                
+                        var guessString = ""
+            
+                        for item in sortedFinishedResults{
+            
+            
+                            guessString += String(format: "x = %f", item.xValue)
+                            guessString += " "
+                            guessString += item.direction
+                            guessString += " "
+                            guessString += String(format: "Bessel = %7.5e", item.besselValue)
+                            guessString += "\n"
+            
+            
+                        }
+            
+             guessString += "End time \(Date())\n"
+            let duration = startTime.distance(to: .now())
+            guessString += "Elapsed time \(duration)\n"
+            
+            // Display the sorted text in the GUI
+                       
+                        await updateGUI(text: guessString)
+               
+                
+                
+            }
+            
+        
+        }
+        
+        //func calculateUpwardDownwardBessel(index: Int, step: Double, xmin: Double, order: Int, start: Int) async {
+        func calculateSinSum(index: Int, step: Double, xmin: Double, order: Int) {
+                
+                let resultsOfTaskCalculation = await withTaskGroup(of: (direction: String, xValue: Double, order: Int, start: Int, besselValue: Double).self /* this is the return from the taskGroup*/,
+                                                                   returning: [(direction: String, xValue: Double, order: Int, start: Int, besselValue: Double)].self /* this is the return of all of the results */,
+                                                                   body: { taskGroup in  /*This is the body of the task*/
+                    
+                    // We can use `taskGroup` to spawn child tasks here.
+                    
+                    
+                    taskGroup.addTask {
+                        
+                        let x = Double(index)*step + xmin
+                        
+                        //Create a new instance of the Bessel Function Calculator object so that each has it's own calculating function to avoid potential issues with reentrancy problem
+                        let downwardResult = await BesselFunctionCalculator().calculateDownwardRecursion(xValue: x, order: order, start: start)
+                        
+                        
+                        return (downwardResult)  /* this is the return from the taskGroup*/
+                        
+                    }
+                    taskGroup.addTask {
+                        
+                        let x = Double(index)*step + xmin
+                        //Create a new instance of the Bessel Function Calculator object so that each has it's own calculating function to avoid potential issues with reentrancy problem
+                        let upperResult = await BesselFunctionCalculator().calculateUpwardRecursion(xValue: x, order: order)
+                        
+                        return (upperResult)  /* this is the return from the taskGroup*/
+                        
+                    }
+                    
+                    
+                    // Collate the results of all child tasks
+                    var combinedTaskResults :[(direction: String, xValue: Double, order: Int, start: Int, besselValue: Double)] = []
+                    for await result in taskGroup {
+                        
+                        combinedTaskResults.append(result)
+                    }
+                    
+                    return combinedTaskResults  /* this is the return from the result collation */
+                    
+                })
+                
+                //Do whatever processing that you need with the returned results of all of the child tasks here.
+                
+                // Sort the results based upon the direction of the result
+                let sortedCombinedResults = resultsOfTaskCalculation.sorted(by: { $0.0 > $1.0 })
+                
+                await updateBesselArray(array: sortedCombinedResults)
+                
+            return
+        }
+        
+        /// updateGUI
+        /// This adds the text String to the text displayed in the GUI.
+        /// This function runs on the Main Thread only
+        /// - Parameter text: text to be added so that it can be displayed
+        @MainActor func updateGUI(text:String){
+            
+            guess += text
+            
+        }
+        
+        @MainActor func updateBesselArray(array:[(direction: String, xValue: Double, order: Int, start: Int, besselValue: Double)]){
+            
+            besselResultArray += array
+            
+        }
+        
+        @MainActor func clearBesselArray(){
+            
+            besselResultArray = []
+            
+           
+            
+        }
+
+
+    }
+
+    #Preview {
+        ContentView()
+    }
--- Sine Convergence and Precision Analysis/Sine Calc.swift
+++ Sine Convergence and Precision Analysis/Sine Calc.swift
@@ -6,3 +6,55 @@
 //
 
 import Foundation
+import Observation
+
+@Observable class sinSum {
+    var N = 0
+    var xVal = 0.0
+    var sinSum = 0.0
+    var prec = false
+    
+    func initSinSum(N: Int, xVal: Double) -> Double {
+        self.N = N
+        self.xVal = xVal
+        
+        for n in 1...N {
+            let numerator = pow(-1, n-1) * pow(Decimal(xVal), 2*n-1)
+            let denominator = Double(2*n-1)
+            sinSum = sinSum + Double(numerator)/denominator
+        }
+        
+        return sinSum
+        
+    }
+    
+    func getPrecision(dec: Int) -> Bool {
+        let nextSum = initSinSum(N: N+1, xVal: xVal)
+        let precision = sinSum * 1/Double(pow(10, dec))
+        
+        if (nextSum <= precision) {
+            prec = true
+            return prec
+        }
+        else {
+            prec = false
+            return prec
+        }
+    }
+    
+    //stops when result is as pecise as desired (i think)
+    func checkPrecision(dec: Int) -> Double {
+        sinSum = 0.0
+        var n = 1
+        while prec == false && n<=N {
+            let numerator = pow(-1, n-1) * pow(Decimal(xVal), 2*n-1)
+            let denominator = Double(2*n-1)
+            sinSum = sinSum + Double(numerator)/denominator
+            let _ = getPrecision(dec: dec)
+            n = n+1
+        }
+        
+        return sinSum
+    }
+    
+}
